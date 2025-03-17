9387376ba403dbbcf85b9ed3f2f3610a37e8c179
oussamazze <oussamaabrougui32@gmail.com>
2025-03-17 20:23:11 +0100

    Ajout du dossier client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+
+import { useDispatch } from "react-redux";
+import { setUser } from "./redux/features/authSlice";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AddEditTour from "./pages/AddEditTour";
+import SingleTour from "./pages/SingleTour";
+import Dashboard from "./pages/Dashboard";
+import TagTours from "./pages/TagTours";
+import NotFound from "./pages/NotFound";
+
+import PrivateRoute from "./components/PrivateRoute";
+import Header from "./components/Header";
 
 function App() {
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  useEffect(() => {
+    if (user) {
+      dispatch(setUser(user));
+    }
+  }, [dispatch, user]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer />
+        <Header /> {/* Ajout du Header ici */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tours/search" element={<Home />} />
+          <Route path="/tours/tag/:tag" element={<TagTours />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/addTour"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/editTour/:id"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/tour/:id" element={<SingleTour />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/features/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
